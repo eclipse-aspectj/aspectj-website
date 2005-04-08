8d7248ab62bc77ba9f465a8f08d91991f732b0f5
acolyer <acolyer>
2005-04-08 08:44:15 +0000

    instead of parsing feed every time page is requested, just included the pre-parsed output from the 'syndication' folder

--- syndication.php
+++ syndication.php
@@ -26,26 +26,7 @@
 	<h2>The Aspects Blog</h2>
    
    <ul>
-	   <?php
-	
-	    include './scripts/lastRSS.php';
-	
-	    $rss = new lastRSS;
-	
-	    $rss->cache_dir='.';
-	    $rss->cache_time=3600;
-	    $rss->date_format='l';
-	    $rss->items_limit = 5;
-	
-	    if ($rs = $rss->get('http://www.aspectprogrammer.org/blogs/adrian/index.xml')) {
-	      foreach ($rs['items'] as $item) {
-	        echo "<li><a href=\"$item[link]\" title=\"$item[title]\" target=\"external\">$item[title]</a></li>\n";
-	      }
-	    } else {
-	      echo "bad feed";
-	    }
-	
-	   ?>
+	   <?php require('syndication/aspectprogrammer.html');  ?>
 	 	<li><a href="http://www.aspectprogrammer.org/blogs/adrian"> more...</a></li>
 	</ul>
 
@@ -53,17 +34,7 @@
 
 	<h2>Aspectivity</h2>
 	<ul>
-	    <?php
-	  
-	      if ($rsram = $rss->get('http://ramnivas.com/blog/wp-rss2.php')) {
-	        foreach ($rsram['items'] as $item) {
-	          echo "<li><a href=\"$item[link]\" title=\"$item[title]\"  target=\"external\">$item[title]</a></li>\n";
-	        }
-	      } else {
-	        echo "bad feed";
-	      }
-	
-	    ?>
+	    <?php require('syndication/aspectivity.html'); ?>
 		<li><a href="http://ramnivas.com/blog/"> more...</a></li>
 	</ul>
 	
@@ -71,17 +42,7 @@
 
 	<h2>Dion Almaer on Tech &amp; AOP</h2>
 	<ul>
-	  <?php
-	  
-	    if ($rsdion = $rss->get('http://www.almaer.com/blog/tech.xml')) {
-	      foreach ($rsdion['items'] as $item) {
-	        echo "<li><a href=\"$item[link]\" title=\"$item[title]\"  target=\"external\">$item[title]</a></li>\n";
-	      }
-	    } else {
-	      echo "bad feed";
-	    }
-	
-	  ?>
+	  <?php require('syndication/dion.html');	  ?>
 	  <li><a href="http://www.almaer.com/blog/archives/cat_aop.html"> more...</a> </li>
   </ul>
   
@@ -89,17 +50,7 @@
 
 	<h2>Ron Bodkin on AOP</h2>
 	<ul>
-	  <?php
-	  
-	    if ($rsron = $rss->get('http://rbodkin.blogs.com/ron_bodkins_blog/index.rdf')) {
-	      foreach ($rsron['items'] as $item) {
-	        echo "<li><a href=\"$item[link]\" title=\"$item[title]\"  target=\"external\">$item[title]</a></li>\n";
-	      }
-	    } else {
-	      echo "bad feed";
-	    }
-	
-	  ?>
+	  <?php require('syndication/ron.html'); ?>
 		<li><a href="http://rbodkin.blogs.com/ron_bodkins_blog/aspectoriented_programming/index.html"> more...</a> </li>
 	</ul>
 
@@ -108,19 +59,7 @@
    <h2>AspectJ in Yahoo news...</h2>
    
    <ul>
-	  <?php
-	
-	    if ($rs = $rss->get('http://news.search.yahoo.com/news/rss?ei=UTF-8&p=aspectj')) {
-	      foreach ($rs['items'] as $item) {
-	       ereg("<!\[CDATA\[(.*)]]>",$item[title], $real_title);
-	       echo "<li><a href=\"$item[link]\" title=\"$real_title[1]\" target=\"external\">$real_title[1]</a></li>\n";
-	     }
-	    } else {
-	      echo "bad feed";
-	    }
-	
-	  ?>
-	   
+	  <?php require('syndication/news.html'); ?>
 	   <li><a href="http://news.search.yahoo.com/search/news/?ei=ISO-8859-1&c=&p=aspectj" target=\"external\">
 	      more from Yahoo...</a>
 	   </li>
