e497e323c95a967f8569308d6ec80476788916b2
acolyer <acolyer>
2005-06-09 15:35:10 +0000

    re-enable alex's blog on the home page...

--- syndication.php
+++ syndication.php
@@ -37,13 +37,11 @@
 
 <!-- ============ Alex's Blog ============== -->
 
-<!-- temporarily remove whilst alex writes some content ;)
 	<h3><a href="http://blogs.codehaus.org/people/avasseur">Alex Vasseur's blog</a></h3>
    
    <ul>
 	   <xphp require('syndication/alex.html');  x>
    </ul>
--->
 
 
 <!-- ============ Aspectivity blog portlet =============== -->
