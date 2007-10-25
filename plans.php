fae1b0fc920f6f1b4ae7f0414a871d1518e3fa4f
aclement <aclement>
2007-10-25 15:12:49 +0000

    plan updated!!!

--- plans.php
+++ plans.php
@@ -12,51 +12,46 @@
 
 <h1>aspectj: development plan</h1>
 
-<p>Last updated 8th November 2006</p>
+<p>Last updated 25th October 2007</p>
+
 
 <h2>AspectJ, version 1.6</h2>
-<p>In the first quarter of 2007 we are planning AspectJ 1.6 - this will incorporate the
-Eclipse 3.2 Java6 compiler.</p>
-
-<h2>AspectJ 5, version 1.5.3</h2>
-
-<p>UPDATE: Release candidate available - 8th November 2006</p>
-<p>The next release of AspectJ will include our new compilation pipeline
-structure which drastically reduces maximum memory requirements for building
-projects from source.  It will also include a number of loadtime weaving
-enhancements to reduce memory consumption.
-
-It is scheduled for release in the 4rd quarter of 2006.  To keep informed,
-watch the <a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=AspectJ&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED">
-    list of open AspectJ bugs</a>, or the
-    <a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=AspectJ&target_milestone=1.5.3">
-        list of AspectJ bugs targetting 1.5.3
-    </a>.
+<p>AspectJ 1.6 is the first AspectJ release to support the Java6 language.</p>
+<p>
+<h3>Themes:</h3>
+<ul>
+<li>Java6 support
+<li>Better support and integration with other common enterprise components: eg. code coverage tools, static code analysis
+tools, code generation libraries
+<li><i>more to come...</i>
+</ul>
+<p>
+<h3>Contents:</h3>
+<ul>
+<li>These <a href="">bugs and enhancements</a> are currently targetted for 1.5.4.
+<li>These <a href="">bugs and enhancements</a> are already fixed, available in todays DEVELOPMENT builds, and 
+will be packaged in the 1.5.4 release.
+</ul>
 </p>
 
+<h2>AspectJ 5, version 1.5.4 - targeted date 7th December 2007</h2>
+<p>AspectJ 1.5.4 is a service refresh of the AspectJ 1.5 codestream.
+</p>
+<p>
+<h3>Contents:</h3>
+<ul>
+<li>These <a href="">bugs and enhancements</a> are currently targetted for 1.5.4.
+<li>These <a href="">bugs and enhancements</a> are already fixed, available in todays DEVELOPMENT builds, and 
+will be packaged in the 1.5.4 release.
+</ul>
+</p>
+
+<h2>AspectJ 5, version 1.5.3 - released 22nd November 2006</h2>
 <h2>AspectJ 5, version 1.5.2 - released 30th June 2006 </h2>
-<p>For the full list of bugs resolved see the 
-<a href="http://www.eclipse.org/aspectj/doc/released/README-152.html"/>AspectJ 1.5.2 README</a>.
-
 <h2>AspectJ 5, version 1.5.1a - released 20th April 2006  </h2>
+<h2>AspectJ 5 - released 20th December 2005</h2>
 
-<p>AspectJ 1.5.1a included numerous bug fixes over 1.5.0 but its' main goal was
-improving memory usage - significant enhancements went in to drastically reduce
-memory used for source compilation.  For the full list of bugs resolved see the 
-<a href="http://www.eclipse.org/aspectj/doc/released/README-151.html"/>AspectJ 1.5.1a README</a>.
-</p>
-
-<h2>AspectJ 5 - Done!</h2>
-
-<p>AspectJ 5 was released on December 20, 2005. For details on the new features,
-see the <a href="http://www.eclipse.org/aspectj/doc/released/adk15notebook">
-AspectJ 5 Developer's Notebook</a>.   The principle goal of AspectJ5 was 
-full integration of the new Java5 language features into the AspectJ language and
-tools.
-</p>
-
-
-  <!-- ================ End of page specific content ===================== -->
+ <!-- ================ End of page specific content ===================== -->
 </div>
 </div>
 </body>
