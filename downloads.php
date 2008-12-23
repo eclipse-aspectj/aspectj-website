634fc475b4dd46afcbca390cf2f3507ed8b57d01
aclement <aclement>
2008-12-23 23:38:49 +0000

    163 final

--- downloads.php
+++ downloads.php
@@ -183,7 +183,7 @@ function ListDevBuilds($dir){
       <td><ul>AspectJ compiler, browser, documentation tool, Ant tasks, and documentation.</ul></td>
       <td width="30%" align="right">
           <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.3rc1.jar">aspectj-1.6.3.jar</a> (~12M)<br>
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.2-src.jar">aspectj-1.6.2-src.jar</a>
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.3-src.jar">aspectj-1.6.3-src.jar</a>
       </td>
     </tr>
     </table>
