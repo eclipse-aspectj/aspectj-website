585fb7dd365e1702aad4d3ae283efc49d6d7f837
aclement <aclement>
2010-01-08 22:59:08 +0000

    1.6.8!

--- downloads.php
+++ downloads.php
@@ -158,13 +158,13 @@ function ListDevBuilds($dir){
   <h3 id="stable_release">Latest Stable Release</h3>
   <ul>
     <li>
-    <b>AspectJ 1.6.7.a, Released 5th January, 2010</b>
+    <b>AspectJ 1.6.8, Released 8th January, 2010</b>
  	<table border="0" cellspacing="0" cellpadding="0" width="100%">
     <tr>
       <td><ul>AspectJ compiler, browser, documentation tool, Ant tasks, and documentation.</ul></td>
       <td width="30%" align="right">
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.7.a.jar">aspectj-1.6.7.a.jar</a> (~12M)<br>
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.7.a-src.jar">aspectj-1.6.7.a-src.jar</a>
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.8.jar">aspectj-1.6.8.jar</a> (~12M)<br>
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.8-src.jar">aspectj-1.6.8-src.jar</a>
       </td>
     </tr>
     </table>
@@ -177,13 +177,13 @@ function ListDevBuilds($dir){
   <h3 id="milestones">AspectJ 6</h3>
   <ul>
     <li>
-    <b>AspectJ 1.6.7.a, Released 5th January, 2010</b>
+    <b>AspectJ 1.6.8, Released 8th January, 2010</b>
     <table border="0" cellspacing="0" cellpadding="0" width="100%">
     <tr>
       <td><ul>AspectJ compiler, browser, documentation tool, Ant tasks, and documentation.</ul></td>
       <td width="30%" align="right">
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.7.a.jar">aspectj-1.6.7.a.jar</a> (~12M)<br>
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.7.a-src.jar">aspectj-1.6.7.a-src.jar</a>
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.8.jar">aspectj-1.6.8.jar</a> (~12M)<br>
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.8-src.jar">aspectj-1.6.8-src.jar</a>
       </td>
     </tr>
     </table>
