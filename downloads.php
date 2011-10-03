bd6d2e25f19294c6c5207573eb878d04c6bdb276
aclement <aclement>
2011-10-03 23:53:03 +0000

    1.6.12.RC1

--- downloads.php
+++ downloads.php
@@ -177,26 +177,13 @@ function ListDevBuilds($dir){
   <h3 id="milestones">AspectJ 6</h3>
   <ul>
   <li>
-    <b>AspectJ 1.6.12.M2, Released 18-Aug-2011</b>
+    <b>AspectJ 1.6.12.RC1, Released 3-Oct-2011</b>
     <table border="0" cellspacing="0" cellpadding="0" width="100%">
     <tr>
       <td><ul>AspectJ compiler, browser, documentation tool, Ant tasks, and documentation.</ul></td>
       <td width="30%" align="right">
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.12.M2.jar">aspectj-1.6.12.M2.jar</a> (~12M)<br>
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.12.M2-src.jar">aspectj-1.6.12.M2-src.jar</a>
-      </td>
-    </tr>
-    </table>
-    </li>
-    
-    <li>
-    <b>AspectJ 1.6.12.M1, Released 7-Jun-2011</b>
-    <table border="0" cellspacing="0" cellpadding="0" width="100%">
-    <tr>
-      <td><ul>AspectJ compiler, browser, documentation tool, Ant tasks, and documentation.</ul></td>
-      <td width="30%" align="right">
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.12.M1.jar">aspectj-1.6.12.M1.jar</a> (~12M)<br>
-          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.12.M1-src.jar">aspectj-1.6.12.M1-src.jar</a>
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.12.RC1.jar">aspectj-1.6.12.RC1.jar</a> (~12M)<br>
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.6.12.RC1-src.jar">aspectj-1.6.12.RC1-src.jar</a>
       </td>
     </tr>
     </table>
