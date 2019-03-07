75c94f86e53ac7ae1a412362caf99e8da03c9b71
Andy Clement <aclement@pivotal.io>
2019-03-07 11:29:49 -0800

    1.9.3.rc1

--- downloads.php
+++ downloads.php
@@ -159,6 +159,25 @@ function ListDevBuilds($dir){
   <h3 id="stable_release">Latest Stable Release</h3>
   <ul>
     <li>
+    <b>AspectJ 1.9.3.RC1, Released 7 Mar 2019</b>
+ 	<table border="0" cellspacing="0" cellpadding="0" width="100%">
+    <tr>
+      <td><ul>AspectJ compiler, browser, documentation tool, Ant tasks, and documentation.</ul></td>
+      <td width="30%" align="right">
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.9.3.RC1.jar">aspectj-1.9.3.RC1.jar</a> (~17M)<br>
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/aspectj/aspectj-1.9.3.RC1-src.jar">aspectj-1.9.3.RC1-src.jar</a>
+      </td>
+    </tr>
+    </table>
+  </li>
+  </ul>
+</div>
+
+  <!-- ============ ASPECTJ MILESTONES -->
+  <div class="homeitem3col">
+  <h3 id="milestones">AspectJ 9</h3>
+  <ul>
+    <li>
     <b>AspectJ 1.9.2, Released 24 Oct 2018</b>
  	<table border="0" cellspacing="0" cellpadding="0" width="100%">
     <tr>
@@ -171,11 +190,6 @@ function ListDevBuilds($dir){
     </table>
   </li>
   </ul>
-</div>
-
-  <!-- ============ ASPECTJ MILESTONES -->
-  <div class="homeitem3col">
-  <h3 id="milestones">AspectJ 9</h3>
   <ul>
     <li>
     <b>AspectJ 1.9.1, Released 20 Apr 2018</b>
--- index.php
+++ index.php
@@ -87,6 +87,12 @@
 		<div class="homeitem">
 			<h3>News and Events</h3>
 
+<li>AspectJ 1.9.3.RC1 is <a href="downloads.php">available for download</a>.
+See the <a href="doc/released/README-193.html">readme</a>
+for more information.
+<br><div align="right"><span class="dates"><i>posted 7-Mar-19</i></span></div>
+</li>
+
 <li>AspectJ 1.9.2 is <a href="downloads.php">available for download</a>.
 See the <a href="doc/released/README-192.html">readme</a>
 for more information.
