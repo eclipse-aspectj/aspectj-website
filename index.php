4fe997e1a03e815cc3f5e1ff0933e7c0c099955b
Andy Clement <aclement@pivotal.io>
2019-04-04 12:55:32 -0700

    1.9.3 release

--- index.php
+++ index.php
@@ -87,6 +87,12 @@
 		<div class="homeitem">
 			<h3>News and Events</h3>
 
+<li>AspectJ 1.9.3 is <a href="downloads.php">available for download</a>.
+See the <a href="doc/released/README-193.html">readme</a>
+for more information.
+<br><div align="right"><span class="dates"><i>posted 4-Apr-19</i></span></div>
+</li>
+
 <li>AspectJ 1.9.3.RC1 is <a href="downloads.php">available for download</a>.
 See the <a href="doc/released/README-193.html">readme</a>
 for more information.
