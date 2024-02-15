5b80e1bb514ddc0dbee5b7708d1facd26bf3d9c5
Alexander Kriegisch <Alexander@Kriegisch.name>
2024-02-15 12:33:07 +0700

    Update team.php
    Add some missing important committer alumni and commit stats. Remove
    separate committers.php, contributors.php, because they are unused.

--- team.php
+++ team.php
@@ -39,7 +39,7 @@
 
     <div class="homeitem">
 
-      <h3>Committers</h3>
+      <h3>Committers (active)</h3>
 
       <table cellpadding="2">
         <tr>
@@ -48,8 +48,13 @@
         </tr>
         <tr>
           <td>Andy Clement</td>
-          <td>IBM UK Ltd</td>
+          <td>Broadcom Inc. (formerly VMware)</td>
         </tr>
+      </table>
+
+      <h3>Committers (alumni)</h3>
+
+      <table cellpadding="2">
         <tr>
           <td>Matthew Webster</td>
           <td>IBM UK Ltd</td>
@@ -70,6 +75,14 @@
           <td><a href="https://kerstens.org/mik">Mik Kersten</a></td>
           <td>University of British Columbia</td>
         </tr>
+        <tr>
+          <td>Jim Hugunin</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Erik Hilsdale</td>
+          <td></td>
+        </tr>
       </table>
 
     </div>
@@ -78,16 +91,47 @@
 
       <h3>Contributors</h3>
 
-      <table cellpadding="2">
-        <tr>
-          <td>Helen Hawkins</td>
-          <td>IBM UK Ltd</td>
-        </tr>
-        <tr>
-          <td>Ron Bodkin</td>
-          <td></td>
-        </tr>
-      </table>
+      <p>
+        Helen Hawkins (IBM UK Ltd), Ron Bodkin and many others who contributed code, documentation or other helpful
+        things to AspectJ. Thanks to all of you!
+      </p>
+      <p>
+        Some Git statistics (from February 2024, commit cbdbba0ab0) - please note the at least 5 incarnations of Andy Clement:
+
+<pre><code>$ git shortlog -sne
+  4772  aclement <aclement>
+   953  acolyer <acolyer>
+   918  wisberg <wisberg>
+   534  Andy Clement <aclement@...>
+   532  Alexander Kriegisch <Alexander@...>
+   313  jhugunin <jhugunin>
+   205  mkersten <mkersten>
+   119  mwebster <mwebster>
+   108  Andy Clement <aclement@...>
+   100  Andy Clement <andrew.clement@...>
+   100  avasseur <avasseur>
+    94  ehilsdal <ehilsdal>
+    41  Lars Grefer <eclipse@...>
+    22  Andy Clement <aclement@...>
+    19  Andrey Turbanov <turbanoff@...>
+    11  hsestupin <stupin.sergey@...>
+     7  Jerry James <loganjerry@...>
+     4  Uri Simchoni <urisimchoni@...>
+     3  Dmitry Mikhaylov <dmitry@...>
+     2  Emmanuel Bourg <ebourg@...>
+     2  eli-fin <eyfinkel@...>
+     1  Andy Russell <arussell123@...>
+     1  Anuj Dodhia <59048795+capgen628@...>
+     1  Jonathan Leitschuh <Jonathan.Leitschuh@...>
+     1  Kui Liu <brucekuiliu@...>
+     1  Semyon Danilov <samvimes@...>
+     1  Stefan Starke <stefan@...>
+     1  Tobias Roeser <le.petit.fou@...>
+     1  jkew <jkew@...></code></pre>
+
+        Not to mention all commits done in other SCM systems before the initial Git import.
+      </p>
+
     </div>
 
     <hr class="clearer"/>
