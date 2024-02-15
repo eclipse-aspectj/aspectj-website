9eb7e7d38fcfebc7833652fce0f52d39ea1263fc
Alexander Kriegisch <Alexander@Kriegisch.name>
2024-02-15 11:23:52 +0700

    Update mailing list information

--- userlists.php
+++ userlists.php
@@ -28,53 +28,22 @@
     <div class="homeitem3col">
       <h3>aspectj-users@eclipse.org</h3>
       <ul>
-        <li><a href="https://dev.eclipse.org/mailman/listinfo/aspectj-users">
-          aspectj-users at eclipse.org</a><br>
-          Supports discussion among AspectJ users and developers on topics such
-          as language design, new features, and best usage practices.
+        <li><a href="https://dev.eclipse.org/mailman/listinfo/aspectj-users">aspectj-users at eclipse.org</a><br>
+          Supports discussion among AspectJ users and developers on topics such as language design,
+          new features, and best usage practices.
         </li>
-
-        <li><a href="https://dev.eclipse.org/mhonarc/lists/aspectj-users/maillist.html">
-          Mailing list archive</a></li>
-
-        <li><a href="https://www.eclipse.org/search/search.cgi?cmd=Search%21&form=extended&ps=10&m=all&t=5&ul=%2Fmhonarc%2Flists%2Faspectj-users&wm=wrd&t=News&t=Mail">Search
-          list</a></li>
-
+        <li><a href="https://dev.eclipse.org/mhonarc/lists/aspectj-users/maillist.html">Mailing list archive</a></li>
+        <li><a href="https://www.mail-archive.com/aspectj-users@eclipse.org/">Search list on mail-archive.com mirror</a></li>
       </ul>
     </div>
 
     <div class="homeitem3col">
       <h3>aspectj-announce@eclipse.org</h3>
       <ul>
-        <li><a href="https://dev.eclipse.org/mailman/listinfo/aspectj-announce">
-          aspectj-announce at eclipse.org</a><br>
-          Used only to announce new releases and other important events. This is
-          a low traffic list.
+        <li><a href="https://dev.eclipse.org/mailman/listinfo/aspectj-announce">aspectj-announce at eclipse.org</a><br>
+          Used only to announce new releases and other important events. This is a low traffic list.
         </li>
-
-        <li><a href="https://dev.eclipse.org/mhonarc/lists/aspectj-announce/maillist.html">
-          Mailing list archive</a></li>
-
-        <li><a href="https://www.eclipse.org/search/search.cgi?cmd=Search%21&form=extended&ps=10&m=all&t=5&ul=%2Fmhonarc%2Flists%2Faspectj-announce&wm=wrd&t=News&t=Mail">Search
-          list</a></li>
-
-      </ul>
-    </div>
-
-    <div class="homeitem3col">
-      <h3>Archives</h3>
-      <ul>
-        <li><a href="https://dev.eclipse.org/viewcvs/indextech.cgi/~checkout~/aspectj-home/archives/users.zip">
-          users at aspectj.org ZIP archive (deprecated mailing list)</a>
-          <br>These archives contain user discussion from August 1999 - December 2002.
-        </li>
-
-        <li><a href="https://dev.eclipse.org/viewcvs/indextech.cgi/~checkout~/aspectj-home/archives/announce.zip">
-          announce at aspectj.org ZIP archive (deprecated mailing list)</a>
-          <br>These archives contain announcement messages from August 1999 -
-          December 2002.
-        </li>
-
+        <li><a href="https://dev.eclipse.org/mhonarc/lists/aspectj-announce/maillist.html">Mailing list archive</a></li>
       </ul>
     </div>
 
